895e617bd6c01960ee06073a14f9b1a82159553a
Thomas OBrien <obrien@lorentz.leidenuniv.nl>
2018-04-29 16:17:03 +0200

    Replaced tabs with spaces in experiment_setup

--- qsoverlay/experiment_setup.py
+++ qsoverlay/experiment_setup.py
@@ -8,84 +8,84 @@ import json
 from .gate_templates import GateData
 from quantumsim.circuit import uniform_noisy_sampler
 
+
 class Setup:
 
-	def __init__(
-		self, filename=None,
-		gate_dic={}, update_rules=[], 
-		qubit_dic={}, gate_set={}):
+    def __init__(
+            self, filename=None,
+            gate_dic={}, update_rules=[],
+            qubit_dic={}, gate_set={}):
 
-		if filename is not None:
-			self.load(filename)
-		else:
-			self.gate_dic = gate_dic
-			self.update_rules = update_rules
-			self.qubit_dic = qubit_dic
-			self.gate_set = gate_set
+        if filename is not None:
+            self.load(filename)
+        else:
+            self.gate_dic = gate_dic
+            self.update_rules = update_rules
+            self.qubit_dic = qubit_dic
+            self.gate_set = gate_set
 
-	def load(self, filename):
-		with open(filename, 'r') as infile:
-			setup_load_format = json.load(infile)
+    def load(self, filename):
+        with open(filename, 'r') as infile:
+            setup_load_format = json.load(infile)
 
-		self.update_rules = setup_load_format['update_rules']
-		self.qubit_dic = setup_load_format['qubit_dic']
+        self.update_rules = setup_load_format['update_rules']
+        self.qubit_dic = setup_load_format['qubit_dic']
 
-		# Currently assumes each qubit uses the same
-		# uniform_noisy_sampler - this needs fixing
+        # Currently assumes each qubit uses the same
+        # uniform_noisy_sampler - this needs fixing
 
-		seed = list(self.qubit_dic.values())[0]['seed']
-		readout_error = list(self.qubit_dic.values())[0]['readout_error']
-		sampler = uniform_noisy_sampler(
-			seed=seed, readout_error=readout_error)
+        seed = list(self.qubit_dic.values())[0]['seed']
+        readout_error = list(self.qubit_dic.values())[0]['readout_error']
+        sampler = uniform_noisy_sampler(
+            seed=seed, readout_error=readout_error)
 
-		for qb_params in self.qubit_dic.values():
-			qb_params['sampler'] = sampler
+        for qb_params in self.qubit_dic.values():
+            qb_params['sampler'] = sampler
 
-		self.gate_set = {
-			tuple(gate['key']): gate['val'] for gate in setup_load_format['gate_set']
-		}
-		for gate in self.gate_set.values():
-			if 'sampler' in gate[0] and gate[0]['sampler'] is True:
-				gate[0]['sampler'] = sampler
+        self.gate_set = {
+            tuple(gate['key']): gate['val'] for gate in setup_load_format['gate_set']
+        }
+        for gate in self.gate_set.values():
+            if 'sampler' in gate[0] and gate[0]['sampler'] is True:
+                gate[0]['sampler'] = sampler
 
-		gd = GateData()
+        gd = GateData()
 
-		self.gate_dic = {
-			key: gd.available_gate_dic[val] 
-			for key, val in setup_load_format['gate_dic'].items()
-		}
+        self.gate_dic = {
+            key: gd.available_gate_dic[val] 
+            for key, val in setup_load_format['gate_dic'].items()
+        }
 
+    def save(self, filename):
+        # Save gate_dic
 
-	def save(self, filename):
-		# Save gate_dic
+        # switch from functions to names of functions in
+        # gate_dic to allow saving
+        gate_dic_save_format = {
+            key: val['name'] 
+            for key, val in self.gate_dic.items()
+        }
 
-		# switch from functions to names of functions in
-		# gate_dic to allow saving
-		gate_dic_save_format = {
-			key: val['name'] 
-			for key, val in self.gate_dic.items()
-		}
+        # remove link to sampler in qubit_dic to allow saving
+        qubit_dic_save_format = {
+            key: {**val} for key, val in self.qubit_dic.items()
+        }
+        for qd in qubit_dic_save_format.values():
+            del qd['sampler']
 
-		# remove link to sampler in qubit_dic to allow saving
-		qubit_dic_save_format = {
-			key: {**val} for key, val in self.qubit_dic.items()
-		}
-		for qd in qubit_dic_save_format.values():
-			del qd['sampler']
+        gate_set_save_format = [
+            {'key': key, 'val': [{**val[0]},{**val[1]}]} 
+            for key, val in self.gate_set.items()
+        ]
+        for gate_desc in gate_set_save_format:
+            if 'sampler' in gate_desc['val'][0]:
+                gate_desc['val'][0]['sampler'] = True
 
-		gate_set_save_format = [
-			{'key': key, 'val': [{**val[0]},{**val[1]}]} 
-			for key, val in self.gate_set.items()
-		]
-		for gate_desc in gate_set_save_format:
-			if 'sampler' in gate_desc['val'][0]:
-				gate_desc['val'][0]['sampler'] = True
-
-		setup_save_format = {
-			'gate_dic': gate_dic_save_format,
-			'update_rules': self.update_rules,
-			'qubit_dic': qubit_dic_save_format,
-			'gate_set': gate_set_save_format
-		}
-		with open(filename, 'w') as outfile:
-			json.dump(setup_save_format, outfile)
+        setup_save_format = {
+            'gate_dic': gate_dic_save_format,
+            'update_rules': self.update_rules,
+            'qubit_dic': qubit_dic_save_format,
+            'gate_set': gate_set_save_format
+        }
+        with open(filename, 'w') as outfile:
+            json.dump(setup_save_format, outfile)
